26cce2c25f2230f65c5b685d2f4e25c1c9c6f350
tumpech <laszlo.tumpech@proton.me>
2025-03-09 09:40:53 +0100

    CH3-5

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const rabbitmqConnectionString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Error prompting user for username: %v", err)
+	}
+
+	rabbitmqConnection, err := amqp.Dial(rabbitmqConnectionString)
+	if err != nil {
+		log.Fatalf("Error connecting to RabbitMQ: %v", err)
+	}
+
+	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+
+	pubsub.DeclareAndBind(
+		rabbitmqConnection,
+		"peril_direct",
+		queueName,
+		routing.PauseKey,
+		pubsub.TransientQueue,
+	)
+
 	fmt.Println("Starting Peril client...")
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigChan
+	log.Printf("Received signal %v. Exiting.\n", sig.String())
+	os.Exit(0)
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -7,6 +7,11 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+const (
+	DurableQueue   = iota // 0
+	TransientQueue        // 1
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	marshalledT, err := json.Marshal(val)
 	if err != nil {
@@ -24,4 +29,58 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		},
 	)
 	return nil
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType int, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	rabbitmqChannel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	var rabbitmqQueue amqp.Queue
+
+	if simpleQueueType == DurableQueue {
+		rabbitmqQueue, err = rabbitmqChannel.QueueDeclare(
+			queueName,
+			true,
+			false,
+			false,
+			false,
+			nil,
+		)
+	} else {
+		rabbitmqQueue, err = rabbitmqChannel.QueueDeclare(
+			queueName,
+			false,
+			true,
+			true,
+			false,
+			nil,
+		)
+	}
+
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = rabbitmqChannel.QueueBind(
+		queueName,
+		key,
+		exchange,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return rabbitmqChannel, rabbitmqQueue, nil
+
 }
